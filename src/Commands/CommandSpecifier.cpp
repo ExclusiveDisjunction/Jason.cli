43c19fc333f44004e422a610436338cbacd58aea
exdisj <exdisj@outlook.com>
2024-10-13 16:00:40 -0400

    Discovered a bug and tried to fix it.

--- src/Commands/CommandSpecifier.cpp
+++ src/Commands/CommandSpecifier.cpp
@@ -48,25 +48,20 @@ CommandSpecifier CommandSpecifier::Parse(std::istream& in)
     if (one != '-' && two != '-') //Not --
         throw std::logic_error("Format error: Could not construct a command specifier from this input.");
 
-    std::string wholeLine;
-    std::getline(in, wholeLine);
-    std::stringstream line(wholeLine);
-    wholeLine.clear();
-
+    std::string nameRaw;
+    in >> nameRaw;
+    auto iter = std::find(nameRaw.begin(), nameRaw.end(), '='); //Looking for a value.
     CommandSpecifier result;
-    std::string& name = result.Name;
-    while (one != '=' && !line.eof())
-    {
-        line >> one;
-        if (one == '=' || line.eof())
-            break;
-        else
-            name += one;
-    }
-    trim(name);
 
-    if (one == '=') //We get the value
-        result.val = CommandValue::Parse(line);
+    if (iter == nameRaw.end())
+        result.Name = nameRaw;
+    else
+    {
+        result.Name = nameRaw.substr(0, nameRaw.end() - iter + 1);
+        std::string value_str = nameRaw.substr(nameRaw.end() - iter + 2);
+        std::stringstream value_stream(value_str);
+        result.val = CommandValue::Parse(value_stream);
+    }
 
     return result;
 }
