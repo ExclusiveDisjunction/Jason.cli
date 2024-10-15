b35fc152477c3a4f42c72ed787275c651ec0b94e
exdisj <exdisj@outlook.com>
2024-10-15 16:02:10 -0400

    Began working on CommandBase inheriting classes. This will be contined once the Expressions library is completed.

--- src/Commands/CommandBase.h
+++ src/Commands/CommandBase.h
@@ -12,18 +12,27 @@
 
 enum CommandTypeE
 {
+    Evaluate,
+    Store,
 
+    Load,
+    Unload,
+    List,
+    Info,
+
+    Export,
+    Import
 };
 class CommandType
 {
 public:
-    CommandType(CommandTypeE type, std::string name, std::optional<std::string> desc = {}) : EType(type), Name(std::move(name)), Description(std::move(desc))
+    CommandType(CommandTypeE type, std::string Name, std::string DisplayName, std::optional<std::string> desc = {}) : EType(type), Name(std::move(Name)), DisplayName(std::move(DisplayName)), Description(std::move(desc))
     {
 
     }
 
     const CommandTypeE EType;
-    const std::string Name;
+    const std::string Name, DisplayName;
     const std::optional<std::string> Description;
 };
 
@@ -41,7 +50,7 @@ public:
     CommandBase& operator=(const CommandBase& obj) noexcept = delete;
     CommandBase& operator=(CommandBase&& obj) noexcept = delete;
 
-    [[nodiscard]] virtual CommandBase* Clone() const noexcept = 0;
+    [[nodiscard]] virtual std::unique_ptr<CommandBase*> Clone() const noexcept = 0;
     [[nodiscard]] virtual const CommandType& Type() const noexcept = 0;
 
     template<typename T> requires std::is_base_of<CommandBase, T>::value
@@ -55,7 +64,7 @@ public:
         return dynamic_cast<const T*>(this);
     }
 
-    [[nodiscard]] virtual bool Execute(Session& On) = 0;
+    [[nodiscard]] virtual bool Execute(Session& On, std::ostream& Output) = 0;
     [[nodiscard]] virtual bool Parse(const CommandParser& Parser) = 0;
 };
 
---
+++ src/Commands/JasonCommands.h
@@ -0,0 +1,57 @@
+//
+// Created by Hollan on 10/15/24.
+//
+
+#ifndef JASON_JASONCOMMANDS_H
+#define JASON_JASONCOMMANDS_H
+
+#include "CommandBase.h"
+
+#include "../Expressions/Expression.h"
+#include "../IO/PackageEntryKey.h"
+
+class EvaluateCommand : public CommandBase
+{
+private:
+    static CommandType ThisType;
+
+public:
+    EvaluateCommand();
+    explicit EvaluateCommand(Expression&& Expr);
+    explicit EvaluateCommand(const Expression& Expr);
+
+    Expression TargetExpression;
+    bool StoreInAns = true;
+
+    [[nodiscard]] std::unique_ptr<CommandBase*> Clone() const noexcept override;
+    [[nodiscard]] const CommandType& Type() const noexcept override;
+
+    [[nodiscard]] bool Execute(Session& On, std::ostream& Output) override;
+    [[nodiscard]] bool Parse(const CommandParser& Parser) override;
+};
+class StoreCommand : public CommandBase
+{
+private:
+    static CommandType ThisType;
+
+public:
+    StoreCommand() = default;
+    StoreCommand(PackageEntryKey Target, Expression&& Expr);
+
+    PackageEntryKey Target;
+    Expression Expression;
+
+    [[nodiscard]] std::unique_ptr<CommandBase*> Clone() const noexcept override;
+    [[nodiscard]] const CommandType& Type() const noexcept override;
+
+    [[nodiscard]] bool Execute(Session& On, std::ostream& Output) override;
+    [[nodiscard]] bool Parse(const CommandParser& Parser) override;
+};
+
+class LoadCommand
+{
+
+};
+
+
+#endif //JASON_JASONCOMMANDS_H
